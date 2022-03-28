b5135402a7ed755f08a6061fb4a9d9e11fc08609
mahmoud94ha <91053929+mahmoud94ha@users.noreply.github.com>
2022-03-28 21:11:24 +0200

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -55,6 +55,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* Advanced tasks */
+int check_sub_tree_Left(const binary_tree_t *node, int max)
+int check_sub_tree_Right(const binary_tree_t *node, int min)
+int binary_tree_is_bst(const binary_tree_t *tree)
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
